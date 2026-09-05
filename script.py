cb913acbad7e3b39b7b8ebce62c7f8109ef948b1
Kush Nayak <kushnayak123@gmail.com>
2026-09-04 21:51:49 -0400

    clean up script

--- script.py
+++ script.py
@@ -4,7 +4,6 @@ import requests
 
 import json
 import subprocess
-import platform
 
 # from https://www.latlong.net/place/purdue-university-in-west-lafayette-34294.html
 PURDUE_COORDS = (40.423710, -86.921242)
@@ -20,13 +19,12 @@ class Host:
         if self.ok:
             self.ok = self.calculate_dist()
 
+            print(f"pinged {self.host} dist: {self.dist_km} rtt min {self.rtt_min} max {self.rtt_max} avg {self.rtt_avg}")
 
     def ping(self, count):
-        flag = "-c" #flag for linux/mac
-        if platform.system() == "Windows":
-            flag = "-n"
-
-        out = subprocess.run(["ping", flag, str(count), "-i", "0.1", self.host], capture_output=True, text=True)
+        out = subprocess.run(["ping", "-c", str(count), "-i", "0.1", self.host],
+                             capture_output=True,
+                             text=True)
 
         if out.returncode != 0:
             print(f"couldn't ping {self.host} : {out.stderr}")
@@ -39,19 +37,15 @@ class Host:
         self.ip = ip_line.split()[2][1:-2]
 
         # e.g. round-trip min/avg/max/stddev = 212.948/213.369/214.287/0.444 ms
-        stat_str = lines[-1]
-
-        stat_list = list(map(float, stat_str.split()[3].split("/")))
+        stat_line = lines[-1]
+        stat_list = list(map(float, stat_line.split()[3].split("/")))
         self.rtt_min = stat_list[0]
         self.rtt_avg = stat_list[1]
         self.rtt_max = stat_list[2]
-        
+
         return True
 
-
     def calculate_dist(self):
-        self.dist = None
-
         resp = requests.get(f"http://ip-api.com/json/{self.ip}")
         if resp.status_code != 200:
             print(f"bad response for {self.host} status code {resp.status_code}")
@@ -82,8 +76,8 @@ rtt_max  = [host.rtt_max for host in hosts]
 plt.scatter(distance, rtt_min, label='RTT min', color='tab:blue', s=15)
 plt.scatter(distance, rtt_max, label='RTT max', color='tab:red', s=15)
 
-plt.xlabel('Distance')
-plt.ylabel('RTT')
+plt.xlabel('Distance (km)')
+plt.ylabel('RTT (ms)')
 plt.title('Distance vs RTT')
 plt.legend()
 plt.tight_layout()
